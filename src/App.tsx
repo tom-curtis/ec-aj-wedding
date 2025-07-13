bb11b974471b50b3844271f79a930305e0856dce
Tom Curtis <thomas.curtis2@ocado.com>
2025-07-13 11:14:12 +0100

    chore: disable invite page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+
 import SaveTheDatePage from "@/pages/SaveTheDatePage.tsx";
-import InvitationPage from "@/pages/InvitationPage.tsx";
 
 function App() {
   return (
     <Routes>
-        <Route element={<SaveTheDatePage />} path="/" />
-        <Route element={<InvitationPage />} path="/invite" />
+      <Route element={<SaveTheDatePage />} path="/" />
     </Routes>
   );
 }
